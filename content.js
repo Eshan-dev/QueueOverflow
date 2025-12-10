9f16bf52f6b0c72deda6ca4a34f4831494e7f8e3
Eshan-dev <ebhatara@gmail.com>
2025-12-10 17:00:08 +0530

    All functions added for manng.in without styling

--- content.js
+++ content.js
@@ -1,7 +1,58 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkIconURL = chrome.runtime.getURL("assets/bookmark.png");
+const key = "xxxSecretxxx";
+
+async function addBookmark(){
+    const url = window.location.href;
+    const problemTitle = document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0].innerText;
+    const obj = {
+        title: problemTitle,
+        url: url
+    };
+    
+    const currentBookMarks = await getCurrentBookMarks();
+    if(currentBookMarks.some(function(e){
+        return e.url === url;
+    }) == false){
+        const updatedBookMarks = [...currentBookMarks,obj];
+        chrome.storage.local.set({[key] : updatedBookMarks},function(){
+            console.log("added");
+            
+        })
+    }
+    // console.log(problemTitle);
+}
+function showButton(){
+    console.log("Trigger");
+    if(document.getElementById("bookmark-icon"))return;
+    const bookmark = this.document.createElement("img");
+    let currentURL = window.location.href;
+
+    bookmark.id = "bookmark-icon";
+    bookmark.src = bookmarkIconURL;
+    bookmark.style.width = "30px";
+    bookmark.style.width = "30px";
+
+    bookmark.addEventListener('click',addBookmark);
+
+    if(currentURL.includes("maang.in/problems") == true){
+        const title = this.document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0];
+        title.parentNode.parentNode.insertBefore(bookmark,title.parentNode.nextSibling);
+    }
+
+}
+
+ function getCurrentBookMarks(){
+    return new Promise(function(resolve,reject){
+        chrome.storage.local.get([key],function(result){
+            console.log(result[key]);
+            resolve(result[key] || []);
+        })
+    })
+}
+
+const observer = new MutationObserver(function(){
+    // observer.disconnect(); 
+    showButton();
+})
+observer.observe(document.body,{childList:true,subtree:true});
+showButton();
